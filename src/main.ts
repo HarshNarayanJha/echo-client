11970533ca3c08e902860fb11871af9174bc5907
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-10-06 16:31:35 +0530

    feat: add halfmoon

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'halfmoon/css/halfmoon.min.css'
+import 'halfmoon/css/cores/halfmoon.cores.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
