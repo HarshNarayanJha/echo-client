f21b5bc9664e1b92b97633cf6556405f50cb9327
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-10-06 09:48:19 +0530

    add nanoid, pinia and unique-names-generator

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
-  const roomId = "xyzwu"
+import { nanoid } from "nanoid";
+import { uniqueNamesGenerator, type Config, adjectives, colors, animals, names } from "unique-names-generator";
+
+const roomId = nanoid(6);
+
+const nameConfig: Config = {
+  length: 2,
+  separator: " ",
+  style: "capital",
+  dictionaries: [adjectives, animals],
+};
+
+const name: string = uniqueNamesGenerator(nameConfig);
 </script>
 
 <template>
   <h1>Welcome to Echo</h1>
   <p>This app creates a private notepad, which you can share with your friends and type in sync!</p>
 
-  <RouterLink :to="`/${roomId}`"><button>Let's Start</button></RouterLink>
+  <RouterLink :to="{ name: 'room', params: { roomId } }"><button>Let's Start</button></RouterLink>
 </template>
