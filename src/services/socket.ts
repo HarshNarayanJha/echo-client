46da079f3815e57c03db95ec02d7d6554b5ab054
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-10-06 12:45:40 +0530

    read .env, upgrade to transposrts websocket and log connection error

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,6 +1,16 @@
 import { io } from "socket.io-client"
-import type { Socket } from "socket.io-client"
+import { Socket } from "socket.io-client"
+import type { ClientToServerEvents, ServerToClientEvents } from "@/types/events"
 
-const socket: Socket = io('http://localhost:5000')
+const SOCKET_SERVER_URL: string = import.meta.env.VITE_SERVER_URL || 'http://localhost:5000'
+
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> =
+  io(SOCKET_SERVER_URL, {
+   transports: ['websocket']
+  })
+
+socket.on("connect_error", (err) => {
+  console.log(err);
+});
 
 export default socket
